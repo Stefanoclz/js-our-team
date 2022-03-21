eef0373cbec42c216a3df1f59a0967062259efe7
Stefano Calzoni <stefanoclz@hotmail.it>
2022-03-21 15:26:46 +0100

    scritto ciclo per creazione contenitore card

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 console.log('JS OK');
 
 const container = document.querySelector('.team-container');
+
+let card = document.createElement('div');
+
+card.classList.add('team-card');
+
+let image = document.createElement('div');
+
+image.classList.add('card-image');
+
+let text = document.createElement('div');
+
+text.classList.add('card-text');
+
+const myTeam = [
+    {
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        foto: "",
+    }
+]
+
+for (let i = 0; i < myTeam.length; i++) {
+    container.appendChild(card);
+}
