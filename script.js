36f02e7ed0253113e8d6b7c39fcfc304b9ee3c09
Stefano Calzoni <stefanoclz@hotmail.it>
2022-03-21 16:11:42 +0100

    modificato script per inserimento dinamico immagini, lavoro sul testo

--- script.js
+++ script.js
@@ -2,61 +2,59 @@ console.log('JS OK');
 
 const container = document.querySelector('.team-container');
 
-let card = document.createElement('div');
-
-card.classList.add('team-card');
-
-let image = document.createElement('div');
-
-image.classList.add('card-image');
-
-let text = document.createElement('div');
-
-text.classList.add('card-text');
-
 const myTeam = [
     {
         nome: "Wayne Barnett",
         ruolo: "Founder & CEO",
-        foto: "",
+        foto: "img/wayne-barnett-founder-ceo.jpg",
     },
 
     {
         nome: "Angela Caroll",
         ruolo: "Chief Editor",
-        foto: "",
+        foto: "img/wayne-barnett-founder-ceo.jpg",
     },
 
     {
         nome: "Angela Lopez",
         ruolo: "Social Media Manager",
-        foto: "",
+        foto: "img/wayne-barnett-founder-ceo.jpg",
     },
 
     {
         nome: "Barbara Ramos",
         ruolo: "Graphic Designer",
-        foto: "",
+        foto: "img/wayne-barnett-founder-ceo.jpg",
     },
 
     {
         nome: "Scott Estrada",
         ruolo: "Developer",
-        foto: "",
+        foto: "img/wayne-barnett-founder-ceo.jpg",
     },
 
     {
         nome: "Walter Gordon",
         ruolo: "Office Manager",
-        foto: "",
+        foto: "img/wayne-barnett-founder-ceo.jpg",
     },
 ];
 
 console.log(myTeam.length);
 
 for (let i = 0; i < myTeam.length; i++) {
+    let card = document.createElement('div');
     container.appendChild(card);
+    card.classList.add('team-card');
+    let image = document.createElement('div');
     card.appendChild(image);
+    image.classList.add('card-image');
+    let memberFoto = document.createElement('img');
+    memberFoto.src = myTeam[i].foto
+    image.appendChild(memberFoto);
+    let text = document.createElement('div');
     card.appendChild(text);
+    text.classList.add('card-text');
 }
+
 
